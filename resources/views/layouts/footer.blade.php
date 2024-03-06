2da9634621b8fcd8e9c7bf0ca15b52b2231bcf46
Henry Lee <henryleeworld@gmail.com>
2024-03-07 00:03:34 +0800

    feat:更新最新資料

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,9 @@
 <footer class="footer">
     <div class="container-fluid">
-        <div class="copyright">
-            &copy; {{ now()->year . __(' by ') . __('Henry Lee') }}
+        <div id="copyright" class="copyright">
+            &copy;<script>
+                document.getElementById("copyright").appendChild(document.createTextNode(new Date().getFullYear()));
+            </script> {{__(' by ') . __('Henry Lee') }}
         </div>
     </div>
 </footer>
